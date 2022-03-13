dcf6058a690e344086d30635a8e9cdda311bdd7c
navesaurus <56160802+navesaurus@users.noreply.github.com>
2022-03-13 11:59:23 +0100

    Update and rename aggregate.py to aggregate_all.py

--- Data Prep/aggregate_all.py
+++ Data Prep/aggregate_all.py
@@ -6,7 +6,6 @@ combined_csv = pd.DataFrame()
 for file in files:
     if file.endswith(".csv"):
         csv = pd.read_csv(file)
-        #combined_csv = pd.concat(csv)
         combined_csv = combined_csv.append(csv, ignore_index=True)
         print(file)
 
