7047ab6cfe18e53125af064e6d518e2cf532eabc
Jean Qussa <jean.qussa@gmail.com>
2022-03-16 14:05:51 +0100

    Remove unneeded file

--- smatch-frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-export async function runSQLQuery(sql) {
-  const res = await fetch("http://localhost:3005/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ request: sql })
-  });
-  return await res.json();
-}
